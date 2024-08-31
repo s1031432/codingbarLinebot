257ad51272c82e3aced153228e8b76ba6f029154
KevinHuang <kaiweih0821@gmail.com>
2024-08-31 13:59:41 +0800

    feat: user can send message to others via linebot

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, FlexSendMessage, TextSendMessage
 load_dotenv()
-CHANNEL_TOKEN = os.getenv("CHANNEL_TOKEN")
+
 GEMINI_APIKEY = os.getenv("GEMINI_APIKEY")
-print("APIKEY = "+GEMINI_APIKEY +'\n')
+CHANNEL_TOKEN = os.getenv("CHANNEL_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 NOTIFY_TOKEN = os.getenv("NOTIFY_TOKEN")
+# your line user id 
 OWNER_ID = os.getenv("OWNER_USER_ID")
+
 users = []
-# print(CHANNEL_SECRET)
+
 line_bot_api = LineBotApi(CHANNEL_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 app = Flask(__name__)
 
-# lineBotHeaders = {'Authorization':'Bearer '+'1Aeb/ToxCw9imZTLMiNi6THGx7agOIm8txJtVUAB1AkubVBnf/e6eJZGMEb945o+24oAPe5uEOBpgWgcxm/Mc8To8un/6CplgwT8zMvxY9NWfmJwyFlr1QBtwUAX82kKBeJ7psXTaUEWMZqAJrORkgdB04t89/1O/w1cDnyilFU=','Content-Type':'application/json'}
+# lineBotHeaders = {'Authorization':'Bearer '+'xxx','Content-Type':'application/json'}
 # body = {
 #     'to':'U2deac20d8bac62a51ea352713a23f2c9',
 #     'messages':[{
@@ -52,29 +54,35 @@ def handle_message(event):
     userInfoUrl = "https://api.line.me/v2/bot/profile/{}".format(user_id)
     userInfo = requests.get(userInfoUrl, headers={'Authorization': "Bearer {}".format(CHANNEL_TOKEN)})
     user = json.loads(userInfo.text)
-    userMessage = {"contents":[{"parts":[{"text":event.message.text}]}]}
+    userMessage = event.message.text
     if user not in users:
         users.append(user)
-    print('\n')
-    print(users)
-    print('\n')
-
+    
+    # show user list who talked to linebot
     if user["userId"] == OWNER_ID and userMessage == "Send Text":
         userIdStr = ""
         for user in users :
             for i in range(-4,0):
                 userIdStr+= user["userId"][i]
             userIdStr += (' '+ user["displayName"]+'\n')
-        replyMesssage = userIdStr
-        
+        replyMessage = userIdStr
+    # if user text starts with userId, send customized message
+    elif user["userId"] == OWNER_ID and userMessage[0:4].isalnum():
+        ownerMessage = userMessage[5:-1]
+        for user in users:
+            if user["userId"][29:33] == userMessage[0:4]:
+                receiveUser = user["userId"]
+                break
+        line_bot_api.push_message(receiveUser, TextSendMessage(text=ownerMessage))
+        replyMessage = "done!" 
     else:
         response = SendTextztoGemini(userMessage)
         geminiResponseString = response["candidates"][0]["content"]["parts"][0]["text"]
         notifySendMessage("User Name: "+ user['displayName'] +'\n'+'UserId: '+user['userId']+'\n'\
                         +"Gemini Reply:" + response["candidates"][0]["content"]["parts"][0]["text"]
                       )
-        replyMesssage = geminiResponseString
-    text_message = TextSendMessage(replyMesssage)
+        replyMessage = geminiResponseString
+    text_message = TextSendMessage(replyMessage)
     line_bot_api.reply_message(event.reply_token, text_message)
     # requests.get(gasNotifyUrl+"?msg="+user["displayName"]+"："+event.message.text, headers={"Authorization": 'Bearer X5TEgso9iQq5lptU259mb3TD8xidwRSSfc3hSQwiJwv'})
     ### fetch data
@@ -95,6 +103,7 @@ def notifySendMessage(message):
     req = requests.request('POST', 'https://notify-api.line.me/api/notify',headers=notifyHeaders, data=notifyBody)
 
 def SendTextztoGemini(message):
+    message = {"contents":[{"parts":[{"text":message}]}]}
     # 將要傳給Gemini的訊息，依照Gemini的API格式包好
     gemini_data = json.dumps(message)
     # result是Gemini給我們的回應，是一個Object
