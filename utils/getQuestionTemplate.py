f0cc5081996a1c81b7aec040f1f72e49b80e7b33
KevinHuang <kaiweih0821@gmail.com>
2024-09-15 14:28:35 +0800

    feat: show the question when user enter exam

---
+++ utils/getQuestionTemplate.py
@@ -0,0 +1,16 @@
+import json
+def getQuestionTemplate(question):
+    print(question)
+    with open('./assets/json/question.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
+        data = json.dumps(data)
+        data = data.replace("$question",question["question"] )
+        data = data.replace("$optionA","A. "+question["option_a"] )
+        data = data.replace("$optionB","B. "+question["option_b"] )
+        data = data.replace("$optionC","C. "+question["option_c"] )
+        data = data.replace("$optionD","D. "+question["option_d"] )
+        data = data.replace("$reference",question["reference"] )
+        data = data.replace("$contributor",question["contributor"] )
+        data = json.loads(data)
+    #print(data)
+    return data
